d55d4a221246b4b96e8396aec211483d8d718a5d
chineduoscar <chinedunnadoziepatrick@gmail.com>
2026-06-28 11:57:12 +0100

    Added a home route

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import subscriptionRoute from './routes/subscription.routes.js'
-import contactRoute from './routes/contact.routes.js'
+import subscriptionRoute from "./routes/subscription.routes.js";
+import contactRoute from "./routes/contact.routes.js";
 
-dotenv.config()
-const app = express()
-app.use(express.json())
-app.use(cors())
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 3000
-const URI = process.env.MONGO_URI
+const PORT = process.env.PORT || 3000;
+const URI = process.env.MONGO_URI;
 
 // Routes
-app.use('/subscribe', subscriptionRoute)
-app.use('/', contactRoute)
+app.use("/subscribe", subscriptionRoute);
+app.use("/", contactRoute);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Server is running",
+  });
+});
 
 const start = async () => {
- try{
-    await mongoose.connect(URI)
-    console.log('Database is running...')
+  try {
+    await mongoose.connect(URI);
+    console.log("Database is running...");
 
-    app.listen(PORT, ()=>{
-        console.log(`server is listening on port ${PORT}`)
-    })
+    app.listen(PORT, () => {
+      console.log(`server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    }catch(error){
-        console.log(error)
-    }
-}
-
-start()
+start();
